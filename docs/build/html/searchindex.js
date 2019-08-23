79d7a6006d2903b3c1c7512a7fa1e7d5b00f22f0
新一 <eason473867143@gmail.com>
2019-08-23 09:46:29 +0800

    测试提交

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{"\u641c\u7d22\u9875\u9762":0,"\u6a21\u5757\u7d22\u5f15":0,"\u7d22\u5f15":0,content:0},titles:["Welcome to amov_wiki&#8217;s documentation!"],titleterms:{amov_wiki:0,document:0,indic:0,tabl:0,welcom:0}})
